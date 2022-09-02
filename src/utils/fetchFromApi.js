5cf692023a07cc7cce82182ed86776f932e9bad1
Lucas-Zhang66 <aaron060713@gmail.com>
2022-09-02 02:12:24 -0400

    first commit

---
+++ src/utils/fetchFromApi.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": "6419e78ea0mshd59d86fe85a318fp19d52ajsncb3478788a22",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
